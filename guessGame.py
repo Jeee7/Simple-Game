0b42391a0a5770c7c3bcb99917d946042eb7fb91
Jee7 <104018070+Jeee7@users.noreply.github.com>
2022-07-03 18:08:44 +0700

    Update guessGame.py

--- guessGame.py
+++ guessGame.py
@@ -2,23 +2,33 @@
 # Suruh user nebak, lalu kasihtau sama apa nggak
 # Kasih tau dia bener atau salah tebakannya
 
+# 1 - 100
+# Let user guess , then let user know is it correct or no
+# Tell user right or wrong 
+
 from ast import If
 import random as rn
 
+# Set Variable for looping
 mulai = True
 
-angka = rn.randint(1, 10)
+# Generate random number from 1 - 100
+angka = rn.randint(1, 100)
+
+# The game started
 while mulai == True:
 
+    # Get user input for guessing
     tebak = int(input('Pilih Angka : '))
 
+    # Some Validation
     if tebak == angka:
-        print('Horray! Tebakan mu benar, angkanya adalah ' + str(angka))
-        mulai = False
+        print('Horray! Tebakan mu benar, angkanya adalah ' + str(angka)) # Will print out if the guess is Correct
+        mulai = False # Change the start variable for looping to False to make it wont loop again
     elif tebak > int(angka):
-        print('Terlalu tinggi. Coba lagi')
+        print('Terlalu tinggi. Coba lagi') # When the guess is too high from the correct number
     elif tebak < int(angka):
-        print('Terlalu rendah. Coba lagi')
+        print('Terlalu rendah. Coba lagi') # When the guess is too low from the correct number
     else:
-        print('Yahhh salah kack, tebakanmu ' + str(tebak),
+        print('Yahhh salah kack, tebakanmu ' + str(tebak), # Let user know his Guess and the Correct number
               '  Yang benar adalah = ' + str(angka))
